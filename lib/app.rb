b2ee3e25ceaa4e32d59760c04fbbe99f924a3243
Linus Mjornstedt <carl.mjornstedt@gmail.com>
2020-09-15 12:40:26 +0100

    added form

--- lib/app.rb
+++ lib/app.rb
@@ -10,17 +10,16 @@ get '/' do
 get '/secret' do
     "This is a secret"
 end
-
+#returns another string
 get '/othersecret' do
   "This is another secret"
 end
-
+#returns random name stored in @name variable, accesses view file.
 get '/random-cat' do
-  p params
   @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
 end
-
+#returns @name, but it is set by the user in the URL e.g.(?name=Linus)
 get '/named-cat' do
   p params
   @name = params[:name]
